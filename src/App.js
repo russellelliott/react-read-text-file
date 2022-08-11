e5efaf7721b3a22103187fac62977b4de656bccb
Russell Elliott <russell@quadsquad.net>
2022-08-11 12:39:11 -0700

    can read a specific file specified in code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+import langFile from "./test-files/languages.txt"
+import educationFile from "./test-files/education.txt"
+
 function App() {
   const showFile = (e) => {
     e.preventDefault();
@@ -13,9 +16,24 @@ function App() {
     reader.readAsText(e.target.files[0]);
   };
 
+  function test(){
+    fetch(langFile)
+      .then(r => r.text())
+      .then(text => {
+        console.log('text decoded:', text);
+        var preview = document.getElementById('show-text');
+        preview.innerHTML = text;
+        //TEXT = text;
+        //alert(text);
+        //alert('text decoded:', text);
+      });
+  }
+
   return (
     <div>
       <input type="file" onChange={showFile} />
+      <button onClick = {test()}>View Text</button>
+      <div id="show-text">Choose text File</div>
     </div>
   );
 }
